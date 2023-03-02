a251a774ef16d5983a2d962f8b5a2a5db3af45af
Isaac Best <bestis@flip3.engr.oregonstate.edu>
2023-03-01 16:47:56 -0800

    added project layout

---
+++ app.js
@@ -0,0 +1,48 @@
+/*
+    SETUP for a simple web app 
+*/
+// Express
+var express = require('express');   // We are using the express library for the web server
+var app     = express();            // We need to instantiate an express object to interact with the server in our code
+PORT        = 17350;                 // Set a port number at the top so it's easy to change in the future
+
+// Database
+var db = require('./database/db-connector');
+
+
+// Handlebars
+var exphbs = require('express-handlebars');
+const { query } = require('express');
+app.engine('.hbs', exphbs({
+    extname: ".hbs"
+}));
+app.set('view engine', '.hbs');
+
+
+/*
+    ROUTES
+*/
+
+
+
+
+
+// app.js
+
+app.get('/', function(req, res)
+    {
+        res.render('index');                    // Note the call to render() and not send(). Using render() ensures the templating engine
+    });   
+
+
+
+
+
+
+
+/*
+    LISTENER
+*/
+app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+});
